f49c7b1c6c1a274c1fd4720d5d9808d8e73c24f4
Andreas Gohr <andi@splitbrain.org>
2024-11-14 16:03:38 +0100

    properly catch errors when reading JSON

--- script.js
+++ script.js
@@ -3,12 +3,18 @@
 (function () {
     const codeblocks = document.querySelectorAll('pre.code.json');
     codeblocks.forEach((codeblock) => {
-        const json = JSON.parse(codeblock.innerText);
-        const viewer = document.createElement('div');
-        jsonTree.create(json, viewer);
-
-        codeblock.innerHTML = '';
-        codeblock.style.whiteSpace = 'normal';
-        codeblock.append(viewer);
+        try {
+            const json = JSON.parse(codeblock.innerText);
+            const viewer = document.createElement('div');
+            jsonTree.create(json, viewer);
+            codeblock.innerHTML = '';
+            codeblock.style.whiteSpace = 'normal';
+            codeblock.append(viewer);
+        } catch (e) {
+            const err = document.createElement('div');
+            err.innerText = e.message;
+            err.className = 'error';
+            codeblock.append(err);
+        }
     });
 })();
